d70f796de6857d7db2fc4cf66b0f305bd662015b
Lawrence Li <hi@lawrenceli.me>
2023-12-22 21:47:51 +0800

    feat: workaround for gemini system prompt

--- src/gemini.ts
+++ src/gemini.ts
@@ -4,16 +4,22 @@ import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from "ai";
 // convert messages from the Vercel AI SDK Format to the format
 // that is expected by the Google GenAI SDK
 // it seems currently not support system prompt...
-const buildGoogleGenAIPrompt = (messages: Message[]) => ({
-  contents: messages
-    .filter(
-      (message) => message.role === "user" || message.role === "assistant",
-    )
-    .map((message) => ({
-      role: message.role === "user" ? "user" : "model",
-      parts: [{ text: message.content }],
-    })),
-});
+const buildGoogleGenAIPrompt = (messages: Message[]) => {
+  // to support prompt by first user input
+  if (messages[0]["role"] === "system" && messages[1]["role"] === "user") {
+    messages[1]["content"] = messages[0]["content"] + messages[1]["content"];
+  }
+  return {
+    contents: messages
+      .filter(
+        (message) => message.role === "user" || message.role === "assistant",
+      )
+      .map((message) => ({
+        role: message.role === "user" ? "user" : "model",
+        parts: [{ text: message.content }],
+      })),
+  };
+};
 
 export default async function gemini(
   apiKey: string,
